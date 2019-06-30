abe9977cbce139caf837dc566b2e66f370daab71
orris27 <gsorris27@gmail.com>
2019-06-30 14:56:51 +0800

    update

--- books/settings.py
+++ books/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'bookmanage',
         'HOST':'127.0.0.1',
         'USER':'root',
-        'PASSWORD':'xxxxx',
+        'PASSWORD':'123456',
         'PORT':'3306',
     }
 }
